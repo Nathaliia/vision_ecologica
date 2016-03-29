a6da69efc682a9290a0807a473a8884f02f28380
Nathalia Camacho <nathaliicam@gmail.com>
2016-03-29 17:41:35 -0500

    Section Home

---
+++ app/views/aboutUs.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.base')
+
+@section('about_us')
+	<div class="header">
+		<div class="logo">
+			<img width="38%" src="{{ asset('assets/images/logotipo.png') }}">
+		</div>
+		<div class="boton_contacto">
+			<a class="enlace_contacto" href="#">CONTACTO</a>
+		</div>
+	</div>
+	<div class="content_home">
+		<span class="titulo_content">LOREM IPSUM DOLOR SIT AMET</span>
+		<p class="text_content">consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip consectka</p>
+	</div>
+
+	<div class="filter"></div>
+@stop
+
+
+@section('about_us')
+	<div class="header">
+		<div class="logo">
+			<img width="38%" src="{{ asset('assets/images/logotipo.png') }}">
+		</div>
+		<div class="boton_contacto">
+			<a class="enlace_contacto" href="#">CONTACTO</a>
+		</div>
+	</div>
+	<div class="content_home">
+		<span class="titulo_content">LOREM IPSUM DOLOR SIT AMET</span>
+		<p class="text_content">consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip consectka</p>
+	</div>
+
+	<div class="filter"></div>
+@stop
---
+++ app/views/layouts/base.blade.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>Vision Ecologica y Ambiental</title>
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/main.css') }}">
+</head>
+<body>
+	<section class="home">
+		@yield('home')
+	</section>
+
+	<section class="about_us">
+		@yield('about_us')
+	</section>
+	
+	<footer>
+
+	</footer>
+</body>
+</html>
